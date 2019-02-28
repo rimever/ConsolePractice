22128fec3aaa0ddf36b2975d009c640a54f1b4f3
rimever <mot_siva_ordin@hotmail.com>
2019-02-28 10:31:05 +0900

    .Net Coreのコンソールアプリケーションの雛形を生成

---
+++ ConsoleDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
